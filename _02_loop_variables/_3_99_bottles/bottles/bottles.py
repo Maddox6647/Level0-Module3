4136e10a5ada5d3975169e6f45afaf9371ba256d
jereycui2008 <jeremycuid@gmail.com>
2024-02-16 16:56:48 -0800

    bobby is the most chuggie of them ALL

---
+++ _02_loop_variables/_3_99_bottles/bottles/bottles.py
@@ -0,0 +1,8 @@
+if __name__ == '__main__':
+    for i in range(99, 1, -1):
+        print('' + str(i) + ' bottles of beer on the wall, ' + str(i) + ' bottles of beer.'
+              ' Take one down and pass it around, ' + str(i) + ' bottles of beer on the wall')
+print('1 bottle of beer on the wall, 1 bottle of beer. '
+      'Take one down and pass it around, no more bottles of beer on the wall')
+print('No more bottles of beer on the wall, no more bottles of beer. '
+      'Go to the store and buy some more, 99 bottles of beer on the wall.')
--- _03_modulo/_2_bullseye/bullseye/bullseye.pyde
+++ _03_modulo/_2_bullseye/bullseye/bullseye.pyde
@@ -1,11 +1,14 @@
 def setup():
     # Set the size of your sketch
-    
+    size(100, 100)
     
     pass
     
 def draw():
     # Starting with the largest ellipse, use a for loop to draw a bullseye with ellipses
+    fill(211, 38, 34)
+    for i in range():
+        ellipse(50, 50, i/3, i/3)
     
     # Use an if statement and modulo to alternate the color of your rings
     
